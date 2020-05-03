ddd7f5cfc2ef82738bc4dee1408fef736e8dd877
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-05-03 17:04:43 -0300

    Create repository style

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+import { Loading } from './styles';
 
-// import { Container } from './styles';
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
 
-export default function Repository({ match }) {
-  return (
-    <div>
-      <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
-    </div>
-  );
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return <h1>Repository</h1>;
+  }
 }
