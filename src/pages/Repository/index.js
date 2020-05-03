019878eccaed29558e53955b497b65e5a06c7be5
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-05-03 16:06:11 -0300

    Access repository name

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Repository() {
+export default function Repository({ match }) {
   return (
     <div>
-      <h1>Repository</h1>
+      <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
     </div>
   );
 }
